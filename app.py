5c411249d14745e48bf9a5c818fced65c799d075
Dhruv Sharma <dhruvsh5467@gmail.com>
2025-03-09 01:03:34 +0530

    balle balle

--- app.py
+++ app.py
@@ -4,14 +4,24 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 # Connect to PostgreSQL
-dbSql = psycopg2.connect(
+dbSqlp = psycopg2.connect(
     host="turntable.proxy.rlwy.net",
     database="railway",
     user="postgres",
     password="sKaxECGNkgUrrmHnsXcPqjIFfzsWpvzE",
     port="40949"
 )
-cursorSql = dbSql.cursor()
+cursorSql = dbSqlp.cursor()
+
+dbSqlc = psycopg2.connect(
+    host="yamabiko.proxy.rlwy.net",
+    database="railway",
+    user="postgres",
+    password="ERwBkGTALKOrvPYzWKLFfZaahGsXqNgE",
+    port="48760"
+)
+cursorSqlc = dbSqlc.cursor()
+
 
 @app.route('/police', methods=['GET', 'POST'])
 def police():
@@ -26,10 +36,28 @@ def police():
             "INSERT INTO police_info (station_id, station_name, location, officer_name) VALUES (%s, %s, %s, %s)",
             (stationId, stationName, location, officerName)
         )
-        dbSql.commit()
+        dbSqlp.commit()
         
         return render_template('police.html')
     return render_template('police.html')
+@app.route('/criminal', methods=['GET', 'POST'])
+def criminal():
+    if request.method == 'POST':
+        criminalId= request.form['criminalId']
+        name = request.form['name']
+        gender = request.form['gender']
+        crimeDescription = request.form['crimeDescription']
+        status = request.form['status']
+        
+        # Insert data into PostgreSQL
+        cursorSqlc.execute(
+            "INSERT INTO criminal (criminalid, name, gender, crimedesc, status) VALUES (%s, %s, %s, %s, %s)",
+            (criminalId, name, gender, crimeDescription, status)
+        )
+        dbSqlc.commit()
+        
+        return render_template('criminal.html')
+    return render_template('criminal.html')
 
 @app.route('/')
 def index():
