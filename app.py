14bc7a559539ceebf6fa3f3e730064e019109c40
JUGADU-GEEKS <kunnusherry@gmail.com>
2025-03-08 22:16:03 +0530

    MySQL Database creation

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 from flask import Flask, render_template, request
+import mysql.connector
 
 app = Flask(__name__)
+
+dbSql = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="kunal1234",
+    database="Police"
+)
+cursorSql = dbSql.cursor()
+
+@app.route('/police', methods=['GET','POST'])
+def police():
+    if request.method=='POST':
+        stationId = request.form['stationId']
+        stationName = request.form['stationName']
+        location = request.form['location'],
+        officerName = request.form['officer']
+        cursorSql.execute("INSERT INTO police_info(station_id, station_name, location, officer_name) VALUES (%s, %s, %s, %s)",(stationId, stationName, location, officerName))
+        dbSql.commit()
+        return render_template('police.html')
+    return render_template('police.html')
 
 @app.route('/')
 def index():
